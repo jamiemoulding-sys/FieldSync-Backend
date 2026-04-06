33a2dd25420dc6303f3ff690b1d69fc43c11c1b4
Jamie <jamiemoulding@outlook.com>
2026-04-06 15:31:59 +0100

    remove sendgrid

--- services/emailService.js
+++ services/emailService.js
@@ -1,6 +1,6 @@
-async function sendEmail() {
-  console.log("Email skipped (no SendGrid)");
-  return true;
-}
-
-module.exports = { sendEmail };
+module.exports = {
+  sendEmail: async () => {
+    console.log("Email disabled");
+    return true;
+  }
+};
